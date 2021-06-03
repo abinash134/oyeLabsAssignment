5c910f2da455bded482c106b3b1bec315c18170e
abinash134 <pradhanabinash134@gmail.com>
2021-06-03 17:53:34 +0530

    Initial Commit

---
+++ oyeLabsAssingment/oyeLabsAssingmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  oyeLabsAssingmentApp.swift
+//  oyeLabsAssingment
+//
+//  Created by Abinash Pradhan on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct oyeLabsAssingmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
